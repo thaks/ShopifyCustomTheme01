47ed58a52bd05f4013c6d10e103ccb0106f66d1a
Mohammed Thahir <thrmlp@gmail.com>
2019-09-02 19:09:14 +0400

    call it a second day!

--- js/script.js
+++ js/script.js
@@ -1,26 +1,75 @@
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 1.5,
-    slidesToScroll: 1,
-    arrows: {
-        prev: '.glider__prev',
-        next: '.glider__next'
-    },
-    draggable: true,
-    rewind: true,
-    // scrollLock: true
-});
+// new Glider(document.querySelector('.glider'), {
+//     slidesToShow: 1.5,
+//     slidesToScroll: 1,
+//     arrows: {
+//         prev: '.glider__prev',
+//         next: '.glider__next'
+//     },
+//     draggable: true,
+//     rewind: true,
+//     // scrollLock: true
+// });
 
 
 
-document.querySelector('.tahir-lang-button').addEventListener('click', (event) => {
+// document.querySelector('.tahir-lang-button').addEventListener('click', (event) => {
 
-    if( event.target.innerHTML == "Arabic") {
-        document.querySelector('html').setAttribute('dir', 'rtl');
-        event.target.innerHTML = "English";
-    } else {
-        document.querySelector('html').setAttribute('dir', 'ltr');
-        event.target.innerHTML = "Arabic";
+//     if( event.target.innerHTML == "Arabic") {
+//         document.querySelector('html').setAttribute('dir', 'rtl');
+//         event.target.innerHTML = "English";
+//     } else {
+//         document.querySelector('html').setAttribute('dir', 'ltr');
+//         event.target.innerHTML = "Arabic";
 
-    }
+//     }
     
-});
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const desktopMenutriggers = document.querySelectorAll('.cool > li');
+
+function handleEnter() {
+
+    console.log("Entering");
+    
+    this.classList.add('trigger-enter');
+    setTimeout(() => {
+        this.classList.add('trigger-enter-active');
+    }, 100);
+}
+
+function handleLeave() {
+    console.log("Leaving");
+    
+    this.classList.remove('trigger-enter');
+    this.classList.remove('trigger-enter-active');    
+
+}
+
+desktopMenutriggers.forEach( trigger => trigger.addEventListener('mouseenter', handleEnter));
+desktopMenutriggers.forEach( trigger => trigger.addEventListener('mouseleave', handleLeave));
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ q
@@ -0,0 +1,47 @@
+[33mcommit e825097ca065f00ce6adc812e6c2c548418724f4[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Mohammed Thahir <thrmlp@gmail.com>
+Date:   Mon Sep 2 08:30:54 2019 +0400
+
+    Added Logo bar
+
+[33mcommit b30a6018e6598b4948439dcc636d01917761467d[m
+Author: Mohammed Thahir <thrmlp@gmail.com>
+Date:   Sun Sep 1 00:15:49 2019 +0400
+
+    calling it a day!
+
+[33mcommit 9648edfa43b70b8d6f13165cb004d9c1049a3531[m
+Author: Mohammed Thahir <thrmlp@gmail.com>
+Date:   Sat Aug 31 23:00:53 2019 +0400
+
+    ready to go, setup the sass folder strcture.
+
+[33mcommit ce8d6490d1faf878276c1b5332f4d7e456b51c1b[m
+Author: Mohammed Thahir <thrmlp@gmail.com>
+Date:   Sat Aug 31 17:07:45 2019 +0400
+
+    Languge button is added.
+
+[33mcommit a32289ac1ec7bea407c85c4969019aa648f06460[m
+Author: Mohammed Thahir <thrmlp@gmail.com>
+Date:   Sat Aug 31 13:22:15 2019 +0400
+
+    Working continues...
+
+[33mcommit 6d7145b7e1e45bad36405327ac0699f63bd56b91[m
+Author: Mohammed Thahir <thrmlp@gmail.com>
+Date:   Sat Aug 31 11:53:35 2019 +0400
+
+    Staging for real work.
+
+[33mcommit 26cd7fe19ef6c01b4369d801a72dabaef250f9e7[m
+Author: Mohammed Thahir <thrmlp@gmail.com>
+Date:   Sat Aug 31 10:42:45 2019 +0400
+
+    Setup Glider.js
+
+[33mcommit ef56099e63156b5b97c0399cd123886fc3c5a8b9[m
+Author: Mohammed Thahir <thrmlp@gmail.com>
+Date:   Sat Aug 31 10:22:10 2019 +0400
+
+    First Commit for Glide.js
